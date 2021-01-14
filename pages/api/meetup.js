48d43267d6fddf3f54093c3a4c8f712042eb0303
Kevin Gorjan <kevingorjan@gmail.com>
2021-01-14 14:51:49 +0100

    fixed json output

--- pages/api/meetup.js
+++ pages/api/meetup.js
@@ -1,8 +1,8 @@
 export default (req, res) => {
     res.statusCode = 200
     res.json({
-      date: '13th of January',
-      meetup: '#6'
+      date: '14th of January',
+      meetup: '#7'
     })
   }
   
